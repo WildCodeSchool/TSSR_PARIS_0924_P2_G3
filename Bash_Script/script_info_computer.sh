37bdc2fc0fe0deae0eaf23e63795bb1ec04774b6
Elghaia <raya.naitbouda@gmail.com>
2024-10-31 15:18:52 +0100

    Update script_info_computer.sh

--- Bash_Script/script_info_computer.sh
+++ Bash_Script/script_info_computer.sh
@@ -1,96 +1,43 @@
 #!bin/bash
 
 #Menu des informations sur l'ordinateur
-# Fonction pour connaitre la version de l'OS
-# Définition des fonctions
-function OS_Version {
-    echo "Version de l'OS : "
-    lsb_release -a
-    
+function ask_continue {
+    while true; do
+        read -p "Voulez-vous effectuer une autre action ? (o/n) : " CONTINUE
+        case $CONTINUE in
+        [oO]) return ;; # Continue le script
+        [nN]) exit ;;   # Quitte le script
+        *) echo "Veuillez entrer 'o' pour oui ou 'n' pour non." ;;
+        esac
+    done
 }
-#Fonction pour connaitre la liste des applications/paquets installées
-function disk_number {
-    echo "Nombre de disque: "
-    df -h
-}
-
-function disk_partition {
-    echo "Partition (nombre, nom, FS, taille) par disque : "
-    lsbkl
-}
-
-function installed_application {
-    echo "Liste des applications/paquets installées "
-    dpkg --get-selections
-    
-}
-# Fonction pour connaitre la liste des services installées
-function running_service {
-    echo "Liste des services en cours d'execution"
-    systemctl list-units --type=service
-    
-}
-# Fonction pour avoir des détails sur le type de CPU, nombre de coeurs, etc
-function local_user {
-    echo "Liste des utilisateurs locaux"
-    cut -d: -f1 /etc/passwd 
-    
-}
-# Fonction pour avoir des détails sur le type de CPU, nombre de coeurs, etc
-function CPU_details {
-    echo "CPU Information :"
-    lscpu 
-}
-#Fonction pour connaitre le total de la RAM
-function total_RAM {
-    echo "Mémoire RAM totale"
-    free -h -t
-}
-# Fonciont pour connaitre l'utilisation de la mémoire RAM
-function RAM_usage {
-    echo "Utilisation de la RAM"
-    free -h
-    
-}
-# Fonciont pour connaitre l'utilisation du disque
-function disk_usage {
-    echo "Utilisation du disque"
-    df -h
-    
-}
-# Fonction pour connaitre l'utilisation du processeur
-function cpu_usage {
-    echo "Utilisation du processeur"
-    
-}
-
 # Définition des couleurs
-    RED='\033[0;31m'
-    BLUE='\033[0;34m'
-    YELLOW='\033[0;33m'
-    NC='\033[0m' # Aucune couleur
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # Aucune couleur
 
 # Menu
 
 while true; do
-    
+
     echo -e "${RED}------- Menu Informations sur l'Ordinateur -------${NC}"
-   
-    echo  ""
+
+    echo ""
 
     echo -e "${BLUE}----- Informations Système -----${NC}"
     echo "1) Version de l'OS"
     echo "2) Nombre de disques"
     echo "3) Partition (nombre, nom, FS, taille) par disque"
 
-    echo  ""
+    echo ""
 
     echo -e "${BLUE}----- Applications et Services -----${NC}"
     echo "4) Liste des applications/paquets installés"
     echo "5) Liste des services en cours d'exécution"
     echo "6) Liste des utilisateurs locaux"
 
-    echo  ""
+    echo ""
 
     echo -e "${BLUE}----- Ressources Système -----${NC}"
     echo "7) Type de CPU, nombre de cœurs, etc."
@@ -99,7 +46,7 @@ while true; do
     echo "10) Utilisation du disque"
     echo "11) Utilisation du processeur"
 
-    echo  ""
+    echo ""
 
     echo "12) Revenir au Menu Précédent"
     echo "13) Quitter le script"
@@ -108,37 +55,52 @@ while true; do
 
     case $choice in
     1)
-        OS_Version
+        echo "Version de l'OS : "
+        ssh $USER@$CLIENT lsb_release -a
+        ask_continue
         ;;
     2)
-        disk_number
+        echo "Nombre de disque: "
+        ssh $USER@$CLIENT df -h
+        ask_continue
         ;;
     3)
-        disk_partition
+        echo "Partition (nombre, nom, FS, taille) par disque : "
+        ssh $USER@$CLIENT lsblk -o NAME,SIZE,FSTYPE,MOUNTPOINT
+        ask_continue
         ;;
     4)
-        installed_application
+        echo "Liste des applications/paquets installées "
+        ssh $USER@$CLIENT dpkg --get-selections 
         ;;
     5)
-        running_service
+        echo "Liste des services en cours d'execution"
+        ssh $USER@$CLIENT systemctl list-units --type=service --state=running
         ;;
     6)
-        local_user
+        echo "Liste des utilisateurs locaux"
+        ssh $USER@$CLIENT cut -d: -f1 /etc/passwd
         ;;
     7)
-        CPU_details
+        echo "CPU Information :"
+        ssh $USER@$CLIENT lscpu
         ;;
     8)
-        total_RAM
+        echo "Mémoire RAM totale"
+        ssh $USER@$CLIENT free -h -t
         ;;
     9)
-        RAM_usage
+        echo "Utilisation de la RAM"
+        ssh $USER@$CLIENT free -h
         ;;
     10)
-        disk_usage
+        echo "Utilisation du disque"
+        ssh $USER@$CLIENT df -h
         ;;
     11)
-        cpu_usage
+        echo "Utilisation du processeur :"
+        ssh $USER@$CLIENT top -b -n1 | grep "Cpu(s)"
+
         ;;
     12)
         break
