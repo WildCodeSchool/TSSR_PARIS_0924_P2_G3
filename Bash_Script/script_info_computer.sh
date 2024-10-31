9875510189d041dcd6a362a0f9d5ce55789d8ff7
Elghaia <raya.naitbouda@gmail.com>
2024-10-31 10:18:10 +0100

    Update script_info_computer.sh

--- Bash_Script/script_info_computer.sh
+++ Bash_Script/script_info_computer.sh
@@ -5,51 +5,57 @@
 # Définition des fonctions
 function OS_Version {
     echo "Version de l'OS : "
+    lsb_release -a
     
 }
 #Fonction pour connaitre la liste des applications/paquets installées
 function disk_number {
     echo "Nombre de disque: "
-    # Ajoutez ici votre code pour connaitre le nombre de disque
+    df -h
 }
 
 function disk_partition {
     echo "Partition (nombre, nom, FS, taille) par disque : "
-    # Ajoutez ici votre code pour connaitre le partionnement des disques
+    lsbkl
 }
 
 function installed_application {
     echo "Liste des applications/paquets installées "
+    dpkg --get-selections
     
 }
 # Fonction pour connaitre la liste des services installées
 function running_service {
     echo "Liste des services en cours d'execution"
+    systemctl list-units --type=service
     
 }
 # Fonction pour avoir des détails sur le type de CPU, nombre de coeurs, etc
 function local_user {
     echo "Liste des utilisateurs locaux"
-    # Ajoutez ici votre code pour connaitre la liste des utilisateurs locaux
+    cut -d: -f1 /etc/passwd 
+    
 }
-
+# Fonction pour avoir des détails sur le type de CPU, nombre de coeurs, etc
 function CPU_details {
     echo "CPU Information :"
-
+    lscpu 
 }
 #Fonction pour connaitre le total de la RAM
 function total_RAM {
     echo "Mémoire RAM totale"
-   
+    free -h -t
 }
 # Fonciont pour connaitre l'utilisation de la mémoire RAM
 function RAM_usage {
     echo "Utilisation de la RAM"
+    free -h
     
 }
 # Fonciont pour connaitre l'utilisation du disque
 function disk_usage {
     echo "Utilisation du disque"
+    df -h
     
 }
 # Fonction pour connaitre l'utilisation du processeur
@@ -69,20 +75,22 @@ function cpu_usage {
 while true; do
     
     echo -e "${RED}------- Menu Informations sur l'Ordinateur -------${NC}"
+   
+    echo  ""
 
     echo -e "${BLUE}----- Informations Système -----${NC}"
     echo "1) Version de l'OS"
     echo "2) Nombre de disques"
     echo "3) Partition (nombre, nom, FS, taille) par disque"
 
-    echo -e "${YELLOW}---------------------------------${NC}"
+    echo  ""
 
     echo -e "${BLUE}----- Applications et Services -----${NC}"
     echo "4) Liste des applications/paquets installés"
     echo "5) Liste des services en cours d'exécution"
     echo "6) Liste des utilisateurs locaux"
 
-    echo -e "${YELLOW}---------------------------------${NC}"
+    echo  ""
 
     echo -e "${BLUE}----- Ressources Système -----${NC}"
     echo "7) Type de CPU, nombre de cœurs, etc."
@@ -91,7 +99,7 @@ while true; do
     echo "10) Utilisation du disque"
     echo "11) Utilisation du processeur"
 
-    echo -e "${YELLOW}---------------------------------${NC}"
+    echo  ""
 
     echo "12) Revenir au Menu Précédent"
     echo "13) Quitter le script"
