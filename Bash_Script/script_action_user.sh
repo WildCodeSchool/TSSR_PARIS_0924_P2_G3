2e5ca6cd7b7243cce9e75ad0e6cef34dfd4d8123
CHRISTIAN CHAMBRAUD <christian.chambraud74@gmail.com>
2024-11-06 17:05:52 +0100

    version final

--- Bash_Script/script_action_user.sh
+++ Bash_Script/script_action_user.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+# Demande les informations de connexion
+function get_connection_info {
+    read -p "Entrez le nom d'utilisateur avec lequel vous souhaitez vous connecter :" USERDISTANT
+    read -p "Entrez l'adresse IP ou le nom d'hôte de la machine distante : " CLIENT
+}
+
+# Commande pour lancer le ssh en fonction des variables 
+function ssh_exe 
+{
+    ssh "$USERDISTANT@$CLIENT" "$1"
+    }
+
+# Fonction de journalisation
+function log {
+    echo "$(date "+%Y-%m-%d %H:%M:%S") - $1" >> $LOG_FILE
+}
+
+# Chemin vers le fichier log
+LOG_FILE="\\wsl.localhost\Ubuntu\home\raya\user.log"
+
 # Demande de renseigner le nom d'utilisateur sur lequel vous voullez agir
 function user_name ()
 {
@@ -29,33 +49,37 @@ function ask_continue ()
 function user_creation 
 {
     clear
+    get_connection_info
+    user_name 
     echo "Création du compte utilisateur local"
-    read -p "Entrez le nom du compte utilisateur à créer : " newUser
-
+    log "Début de la création de l'utilisateur $USERNAME sur $CLIENT"
+    ssh_exe <<EOF
 #Vérification de l'existence du compte
-if grep -w $newUser /etc/passwd > /dev/null
+if grep -w $USERNAME /etc/passwd > /dev/null
 then
-    #Le compte $newUser existe déjà
+    #Le compte $USERNAMEexiste déjà
     #Afficher message et renvoi au menu précédent
-    echo -e "Le compte utilisateur $newUser existe déjà"
+    echo -e "Le compte utilisateur $USERNAME existe déjà"
 
 else
-    #Le compte $newUser n'existe pas
+    #Le compte $USERNAME n'existe pas
     #Afficher un message et créé le compte utilisateur
    
-    echo "Création de l'utilisateur $newUser"
-    ssh -t $USER@$CLIENT sudo useradd $newUser > /dev/null 
+    echo "Création de l'utilisateur $USERNAME"
+    ssh -t $USER@$CLIENT sudo useradd $USERNAME > /dev/null 
  
     #Vérification de la création du compte utilisateur
-    if cat /etc/passwd | grep $newUser > /dev/null
+    if cat /etc/passwd | grep $USERNAME > /dev/null
     then
 
     #Le compte utilisateur a été créé   
-       echo "Compte utilisateur $newUser créé!"
+       echo "Compte utilisateur $USERNAME créé!"
     else
-       echo "Compte utilisateur $newUser non-créé!"
+       echo "Compte utilisateur $USERNAME non-créé!"
     fi
 fi
+EOF
+    log " Fin de création du compte utilisateur $USERNAME sur $CLIENT"
     ask_continue
 }
 #Fonction qui permet de changer un mot de passe
@@ -66,91 +90,111 @@ function password_change
 
 while 
         clear
+        get_connection_info
         user_name 
+        log "Début du changement du mot de passe de l'utilisateur $USERNAME sur $CLIENT"
+        ssh_exe <<EOF
         ! grep -w "$USERNAME" /etc/passwd > /dev/null
 #Le compte n'existe pas
 do
     echo "Le compte utilisateur $USERNAME n'existe pas."
 #Le compte existe
 done
-   ssh -t $USER@$CLIENT sudo passwd $USERNAME
+    sudo passwd $USERNAME
+EOF
+log "Fin du changement de mot de passe de l'utilisateur $USERNAME sur $CLIENT"
+ask_continue
 
 }
 #Fonction qui permet de supprimer un compte utilisateur
 function user_deletion 
 {
     clear
+    get_connection_info
     echo "Suppression de compte utilisateur local"
-    read -p "Entrez un nom du compte utilisateur à supprimer : " supUser
+    user_name
+    log "Début de suppression de compte utilisateur $USERNAME sur $CLIENT"
+    ssh_exe <<EOF
 #Tester l'existence du compte dans le système
-    if grep -w $supUser /etc/passwd > /dev/null
+    if grep -w $USERNAME /etc/passwd > /dev/null
     then
 #Le compte existe
 #Validation de la suppression 
-        read -p "Etes vous sûre de vouloir supprimer le compte utilisateur $supUser  ? (o/n) : " CONTINUE
+        read -p "Etes vous sûre de vouloir supprimer le compte utilisateur $USERNAME  ? (o/n) : " CONTINUE
         case $CONTINUE in
-            [oO]) sudo userdel -r -f $supUser > /dev/null 2> /dev/null ;;  # Continue le script
+            [oO]) sudo userdel -r -f $USERNAME > /dev/null 2> /dev/null ;;  # Continue le script
             [nN]) exit ;;    # Quitte le script
             *) echo "Veuillez entrer 'o' pour oui ou 'n' pour non." ;;
         esac
-   if grep -w $supUser /etc/passwd > /dev/null
+   if grep -w $USERNAME /etc/passwd > /dev/null
    then
        #Erreur le compte utilisateur non supprimé
-       echo "Attention le compte utilisateur $supUser n'a pas pu être supprimé ! "
+       echo "Attention le compte utilisateur $USERNAME n'a pas pu être supprimé ! "
    else
        #Le compte utilisateur est supprimé
-       echo "Le compte utilisateur $supUser est supprimé ! "
+       echo "Le compte utilisateur $USERNAME est supprimé ! "
    fi
 else
    #Le compte utilisateur n'existe pas
-   echo "Le compte utilisateur $supUser n'exite pas "
+   echo "Le compte utilisateur $USERNAME n'exite pas "
 fi
-
-ask_continue
+EOF
+    log "Fin de suppression de compte utilisateur $USERNAME sur $CLIENT"
+    ask_continue
 }
 # Définition des fonctions qui vont agir sur l'utilisateur
 # Fonction de désactivation du compte utlisateur
 function user_disable 
 {
     clear
+    get_connection_info
     echo "Désactivation de compte utilisateur local"
-    read -p "Entrez le nom du compte utilisateur à désactiver : " desUser
+    user_name
+    log "Début de désactivation de compte utilisateur $USERNAME sur $CLIENT"
+    ssh_exe <<EOF
 #Tester l'existance du compte dans le système
-    if grep -w $desUser /etc/passwd > /dev/null
+    if grep -w $USERNAME /etc/passwd > /dev/null
     then
 #Le compte existe
 #Validation de la désactivation
-        read -p "Etes vous sûre de vouloir désactiver le compte utilisateur $desUser  ? (o/n) : " CONTINUE
+        read -p "Etes vous sûre de vouloir désactiver le compte utilisateur $USERNAME ? (o/n) : " CONTINUE
         case $CONTINUE in
-            [oO]) sudo passwd -l $desUser > /dev/null 2> /dev/null ;;  # Continue le script
+            [oO]) sudo passwd -l $USERNAME > /dev/null 2> /dev/null ;;  # Continue le script
             [nN]) exit ;;    # Quitte le script
             *) echo "Veuillez entrer 'o' pour oui ou 'n' pour non." ;;
         esac
-   if ssh -t $USER@$CLIENT sudo passwd -S $desUser | grep -q "L"
+   if 
+        sudo passwd -S $USERNAMEr | grep -q "L"
    then
        #Le compte utilisateur est désactivé
-       echo "Le compte utilisateur $desUser est désactivé ! " 
+       echo "Le compte utilisateur $USERNAME est désactivé ! " 
     else
        #Erreur le compte utilisateur n'est pas désactivé
-       echo "Attention le compte utilisateur $desUser n'a pas pu être désactivé ! "
+       echo "Attention le compte utilisateur $USERNAME n'a pas pu être désactivé ! "
    fi
 else
    #Le compte utilisateur n'existe pas
-   echo "Le compte utilisateur $desUser n'existe pas "
+   echo "Le compte utilisateur $USERNAME n'existe pas "
 fi
-ask_continue
+EOF
+    log "Fin de désactivation de compte utilisateur $USERNAME sur $CLIENT"
+    ask_continue
 }
 
 # Définition des fonctions qui vont agir sur le groupe
 # Fonction qui permet de rajouter un compte utilisateur à un groupe local
 
-function add_group {
+function add_group 
+{
 #Quel nom d'utilisateur $USERNAME on veut mettre dans un group $GROUPNAME
 #Vérification de l'existence du compte
 
 while 
     clear
-    user_name 
+    get_connection_info
+    user_name
+    log "Début de l'ajout de compte utilisateur $USERNAME dans le groupe $GROUPNAME sur $CLIENT"
+    ssh_exe <<EOF
     ! grep -w "$USERNAME" /etc/passwd > /dev/null
 #Le compte n'existe pas
 do
@@ -166,14 +210,15 @@ done
 #Le groupe existe
     done
         if
-           ssh -t $USER@$CLIENT sudo usermod -aG "$GROUPNAME" "$USERNAME"
+            sudo usermod -aG "$GROUPNAME" "$USERNAME"
         then
             echo "Le compte utilisateur $USERNAME a été ajouté au groupe $GROUPNAME avec succès."
         else
             echo "Échec de l'ajout du compte utilisateur $USERNAME au groupe $GROUPNAME."
         fi
-
-ask_continue
+EOF
+    log "Fin de l'ajout de compte utilisateur $USERNAME dans le groupe $GROUPNAME sur $CLIENT"
+    ask_continue
 }
 
 function delete_group 
@@ -183,7 +228,10 @@ function delete_group
 
 while 
     clear
+    get_connection_info
     user_name 
+    log "Début de retrait de compte utilisateur $USERNAME dans le groupe $GROUPNAME sur $CLIENT"
+    ssh_exe <<EOF
     ! grep -w "$USERNAME" /etc/passwd > /dev/null
 #Le compte n'existe pas
 do
@@ -199,16 +247,15 @@ done
 #Le groupe existe
     done
         if
-            ssh -t $USER@$CLIENT sudo gpasswd -d "$USERNAME" "$GROUPNAME"
+            sudo gpasswd -d "$USERNAME" "$GROUPNAME"
         then
             echo "Le compte utilisateur $USERNAME a été retiré du groupe $GROUPNAME avec succès."
         else
             echo "Échec du retrait du compte utilisateur $USERNAME du groupe $GROUPNAME."
         fi
-
-
-
-ask_continue
+EOF
+    log "Fin de retrait de compte utilisateur $USERNAME dans le groupe $GROUPNAME sur $CLIENT"
+    ask_continue
 }
 
 # Définition des couleurs
